5d8e393851d2675fe85c99546d61ae80f85ad41c
Hadiqa Hanif <hadiqaa201@gmail.com>
2026-07-17 12:56:26 +0500

    factorial

--- factorial.py
+++ factorial.py
@@ -1,6 +1,11 @@
 n = int(input("Enter a number: "))
-factorial = 1
-for i in range(1, n + 1):
-    factorial = factorial * i
-print("Factorial is:", factorial)
+if n < 0 :
+    print("Factorial is not defined for negative numbers.")
+elif n == 0 :
+    print("Factorial is: 1")
+else:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial = factorial * i
+    print("Factorial is:", factorial)
 
